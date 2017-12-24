c87887a3a226c85565311f79063f8fd69cbf1c25
Mincka <julien.ehrhart@live.com>
2017-12-24 11:59:30 +0100

    Add project files.

---
+++ AspNetCoreCracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AspNetCoreCracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
